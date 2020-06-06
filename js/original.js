eaf5d6fba978e98e275e2d7041c60f9c6ac3b92c
Tereh <lositsky@outlook.com>
2020-06-06 17:11:31 +0300

    task completed

--- js/original.js
+++ js/original.js
@@ -21,38 +21,64 @@
 
 Проверить, чтобы все работало без ошибок в консоли */
 
-'use strict';
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели ?', '');
+"use strict";
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели ?", "");
 
 const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
 };
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-        b = prompt('На сколько оцените его?', '');
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('Збережено');
-    } else {
-        alert('Дайте реальну відповідь');
-        i--;
-    }
-}
+// for (let i = 0; i < 2; i++) {
+//   const a = prompt("Один из последних просмотренных фильмов?", ""),
+//     b = prompt("На сколько оцените его?", "");
+//   if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+//     personalMovieDB.movies[a] = b;
+//     console.log("Збережено");
+//   } else {
+//     alert("Дайте реальну відповідь");
+//     i--;
+//   }
+// }
+
+let i = 0;
+// while (i < 2) {
+//   const a = prompt("Один из последних просмотренных фильмов?", ""),
+//     b = prompt("На сколько оцените его?", "");
+//   if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+//     personalMovieDB.movies[a] = b;
+//     console.log("Збережено");
+//   } else {
+//     alert("Дайте реальну відповідь");
+//     i--;
+//   }
+//   i++;
+// }
+do {
+  const a = prompt("Один из последних просмотренных фильмов?", ""),
+    b = prompt("На сколько оцените его?", "");
+  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log("Збережено");
+  } else {
+    alert("Дайте реальну відповідь");
+    i--;
+  }
+  i++;
+} while (i < 2);
+
 console.log(personalMovieDB);
 
-
 if (numberOfFilms < 10) {
-    console.log('Просмотрено довольно мало фильмов')
+  console.log("Просмотрено довольно мало фильмов");
 } else if (numberOfFilms >= 10 && numberOfFilms <= 30) {
-    console.log('Вы классический зритель')
+  console.log("Вы классический зритель");
 } else if (numberOfFilms > 30) {
-    console.log('Вы киноман')
-} else(console.log('Щось пішло не так'))
+  console.log("Вы киноман");
+} else console.log("Щось пішло не так");
 
 /* Задание на урок:
 
